4f1fdff8c0363352264cfbea8b8e056eef994b20
Ekaterina Bagaeva <katrinbag@mail.ru>
2024-03-13 10:59:33 +0100

    add buttons

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 let input = document.querySelector('.input');
 let btnAction = document.getElementById('btnAction');
 let result = document.querySelector('.result');
+
 let btnClear = document.getElementById('btnClear');
 let btnCopy = document.getElementById('btnCopy');
+let btnGPH = document.getElementById('btnGPH');
+let btnZP = document.getElementById('btnZP');
+
+let textGPH = document.getElementById('textGPH');
+let textZP = document.getElementById('textZP');
 
 input.oninput = function (){
   let path = input.value.trim();
@@ -11,19 +17,27 @@ input.oninput = function (){
   result.innerHTML = finalPath;
 }
 
-// btnClear.addEventListener('click', function () {
-//   input.value =''
-//   result.innerHTML = ''
-// })
-
-btnCopy.addEventListener('click', function (){
-  navigator.clipboard.writeText(result.innerHTML)
+function copy(text) {
+  navigator.clipboard.writeText(text.innerHTML)
     .then(() => { })
     .catch((error) => {
       console.error('Error copying text: ', error);
       alert('Error copying text. Please try again.');
     });
+}
+
+btnCopy.addEventListener('click', function (){
+  copy(result)
   input.value =''
   result.innerHTML = ''
 })
 
+
+
+btnGPH.addEventListener('click', function(){
+  copy(textGPH)
+})
+
+btnZP.addEventListener('click', function(){
+  copy(textZP)
+})
